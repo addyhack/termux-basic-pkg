417dea53b995453f130c011ba7953e4e2fc77957
Addy Stark <106424702+addyhack@users.noreply.github.com>
2022-06-01 20:48:50 +0530

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,34 @@
+echo "A sript writen by addy stark";
+
+ echo "WELLCOME TO ANY-HACKER-WORLD";
+ pkg  update -y
+
+ pkg  upgrade -y
+
+ apt-get update -y
+
+ apt-get upgrade -y
+
+ apt-get install git -y
+
+ apt-get install wget -y
+
+ apt-get install curl -y
+
+ apt-get install python -y
+
+ apt-get install python2 -y
+
+ apt-get install python3 -y
+
+ apt-get install php -y
+
+ apt-get install ruby -y
+
+ apt-get install nano -y
+
+ apt-get install apache2 -y
+
+ apt-get install nmap -y
+
+ echo "THANKS FOR USING MY SCRITP";
